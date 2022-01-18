c45642f4a96e570b2035db90208a28800a488a1e
Edward Paowz <gpaowz@gmail.com>
2022-01-18 13:54:58 +0100

    Delegated output

--- lib/TimedMarker.hpp
+++ lib/TimedMarker.hpp
@@ -3,78 +3,72 @@
 #define TIME_MARKER_H
 
 #include <chrono>
-#include <iostream>
-#include <iomanip>
-#include <type_traits>
 #include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <type_traits>
 
 namespace TM {
 
 #if __cplusplus < 201703 || (defined(_MSVC_LANG) && _MSVC_LANG < 201703L)
-    #error "Need C++17 compiler at least !"
+#error "Need C++17 compiler at least !"
 #endif
 
-constexpr const int padding{15};
+constexpr const int padding{25};
 constexpr const int precision{2};
 
-using min   = std::ratio<3600>;
-using sec   = std::ratio<60>;
+using min = std::ratio<3600>;
+using sec = std::ratio<60>;
 using milli = std::milli;
 using micro = std::micro;
-using nano  = std::nano;
+using nano = std::nano;
 
-#define START_TIMER(unit)                                   \
-  using t_unit = unit ;                                     \
-  using namespace std::chrono;                              \
+#define START_TIMER(unit)                                       \
+  using t_unit = unit;                                          \
+  using namespace std::chrono;                                  \
   auto start_total = std::chrono::high_resolution_clock::now(); \
-  {                                                         \
-    static_assert(std::is_same_v<t_unit, TM::min>             \
-                    || std::is_same_v<t_unit, TM::sec>        \
-                    || std::is_same_v<t_unit, TM::milli>      \
-                    || std::is_same_v<t_unit, TM::micro>      \
-                    || std::is_same_v<t_unit, TM::nano>       , "wrong unit");\
-    auto start = start_total;
+  {                                                             \
+    static_assert(std::is_same_v<t_unit, TM::min> ||            \
+                      std::is_same_v<t_unit, TM::sec> ||        \
+                      std::is_same_v<t_unit, TM::milli> ||      \
+                      std::is_same_v<t_unit, TM::micro> ||      \
+                      std::is_same_v<t_unit, TM::nano>,         \
+                  "wrong unit");                                \
+    auto start_block = start_total;
 
-#define LAP(msg)                                                        \
-    duration<double, t_unit> time_block                                 \
-        = std::chrono::high_resolution_clock::now() - start;            \
-    std::cout << std::setw(3) << "»"                                    \
-        << std::setw(TM::padding) << msg << ": "                        \
-        << std::setprecision(TM::precision)                             \
-        << std::fixed << time_block.count()                             \
-        << " " << TM::GetUnitExt<t_unit>() << std::endl;                \
-    }                                                                   \
-    {                                                                   \
-    auto start = std::chrono::high_resolution_clock::now();
+#define LAP(msg)                                               \
+  duration<double, t_unit> time_block =                        \
+      std::chrono::high_resolution_clock::now() - start_block; \
+  TM::Output<t_unit>(msg, time_block.count());                 \
+  }                                                            \
+  {                                                            \
+    auto start_block = std::chrono::high_resolution_clock::now();
 
+#define END_TIMER(msg)                                      \
+  auto end = std::chrono::high_resolution_clock::now();     \
+  duration<double, t_unit> time_block = end - start_block;  \
+  duration<double, t_unit> time_total = end - start_total;  \
+  TM::Output<t_unit>(msg, time_block.count());              \
+  std::cout << "----------------------------" << std::endl; \
+  TM::Output<t_unit>("Total", time_total.count());          \
+  }
 
-#define END_TIMER(msg)                                                  \
-    auto end = std::chrono::high_resolution_clock::now();               \
-    duration<double, t_unit> time_block = end - start;                  \
-    duration<double, t_unit> time_total = end - start_total;            \
-    std::cout << std::setw(3) << "»"                                    \
-        << std::setw(TM::padding) << msg << ": "                        \
-        << std::setprecision(TM::precision)                             \
-        << std::fixed << time_block.count()                             \
-        << " " << TM::GetUnitExt<t_unit>() << std::endl;                \
-    std::cout << "----------------------------" << std::endl            \
-        << std::setw(3) << "»" << std::setw(TM::padding) << "Total: "   \
-        << std::setprecision(TM::precision)                             \
-        << std::fixed << time_total.count()                             \
-        << " " << TM::GetUnitExt<t_unit>() << std::endl;                \
-    }
-
-template<typename T>
-inline constexpr const char* GetUnitExt(){
-    if constexpr(std::is_same_v<T, TM::min>)     return "min";
-    if constexpr(std::is_same_v<T, TM::sec>)     return "s";
-    if constexpr(std::is_same_v<T, TM::milli>)   return "ms";
-    if constexpr(std::is_same_v<T, TM::micro>)   return "µs";
-    if constexpr(std::is_same_v<T, TM::nano>)    return "nano";
-    return "";
+template <typename T>
+inline constexpr const char* GetUnitExt() noexcept {
+  if constexpr (std::is_same_v<T, TM::min>) return "min";
+  if constexpr (std::is_same_v<T, TM::sec>) return "s";
+  if constexpr (std::is_same_v<T, TM::milli>) return "ms";
+  if constexpr (std::is_same_v<T, TM::micro>) return "µs";
+  if constexpr (std::is_same_v<T, TM::nano>) return "ns";
+  return "";
 }
 
-}   // end of namespace
+template <typename T>
+inline void Output(const char* msg, const double& timing) {
+  std::cout << std::setw(3) << "»" << std::setw(TM::padding) << msg << ": "
+            << std::setprecision(TM::precision) << std::fixed << timing << " "
+            << TM::GetUnitExt<T>() << std::endl;
+}
+
+}  // namespace TM
 #endif
-
-
--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,16 @@
 
 #include "TimedMarker.hpp"
 
-int main()
-{
+int main() {
+  START_TIMER(TM::milli)
+  std::cout << "Hello World!" << std::endl;
 
-    START_TIMER(TM::milli)
-    std::cout << "Hello World!"
-              << std::endl;
+  for (int i = 0, j = 0; i < 100000000; ++i) j++;
 
-    for(int i=0,j=0;i<100000000;++i)
-        j++;
+  LAP("intermediate ffff")
 
-    LAP("intermediate")
+  for (int i = 0, j = 0; i < 100000000; ++i) j++;
 
-    for(int i=0,j=0;i<100000000;++i)
-        j++;
-
-    END_TIMER("Time exec")
-    return 0;
+  END_TIMER("Time exec")
+  return 0;
 }
-
